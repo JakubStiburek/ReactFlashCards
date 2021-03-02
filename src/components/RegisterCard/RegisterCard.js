3decfb8135a8e21023021dfcb4e1d3283288519b
Jakub Stiburek <alcolp96@gmail.com>
2021-03-02 09:28:39 +0100

    Comments.

--- src/components/RegisterCard/RegisterCard.js
+++ src/components/RegisterCard/RegisterCard.js
@@ -2,19 +2,25 @@ import React, {useState} from "react"
 import "./RegisterrCard.css"
 
 const RegisterCard = (props) => {
+    // Get memory from props
+    // Set state on input and memory
     const [input, setInput] = useState("")
     const [memory, setMemory] = useState(props.memory)
+    // Store submitted text in memory then clean the input
     const handleSubmit = () => {
         memory.push(input)
         setMemory(memory)
         handleClear()
     }
+    // Clear input
     const handleClear = () => {
         setInput("")
     }
+    // Change input
     const handleInputChange = (event) => {
         setInput(event.target.value)
     }
+    // When memory contains items format it to string
     const formatMemory = () => {
         if(memory.length > 0) {
             const newMemory = memory.join(", ")
