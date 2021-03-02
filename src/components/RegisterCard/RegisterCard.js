7803d77ca80576330e25869322541fcd7ffdf0bc
Jakub Stiburek <alcolp96@gmail.com>
2021-03-02 09:23:30 +0100

    Formating input text and pinting it for the user.

--- src/components/RegisterCard/RegisterCard.js
+++ src/components/RegisterCard/RegisterCard.js
@@ -3,9 +3,11 @@ import "./RegisterrCard.css"
 
 const RegisterCard = (props) => {
     const [input, setInput] = useState("")
+    const [memory, setMemory] = useState(props.memory)
     const handleSubmit = () => {
-        props.memory.push(input)
-        console.log(props.memory)
+        memory.push(input)
+        setMemory(memory)
+        handleClear()
     }
     const handleClear = () => {
         setInput("")
@@ -13,12 +15,19 @@ const RegisterCard = (props) => {
     const handleInputChange = (event) => {
         setInput(event.target.value)
     }
+    const formatMemory = () => {
+        if(memory.length > 0) {
+            const newMemory = memory.join(", ")
+            return newMemory.toUpperCase()
+        }
+    }
 
     return (
         <div>
             <input type="text" value={input} onChange={handleInputChange} />
             <button onClick={handleSubmit}>Submit</button>
             <button onClick={handleClear}>Clear</button>
+            {formatMemory()}
         </div>
     )
 }
