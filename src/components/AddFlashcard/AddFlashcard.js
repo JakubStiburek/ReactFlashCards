8085df4ece390f2772186d9a8733109237b6b9c1
Jakub Stiburek <alcolp96@gmail.com>
2021-03-09 09:34:09 +0100

    Fixed state problem in AddFlashcard.

--- src/components/AddFlashcard/AddFlashcard.js
+++ src/components/AddFlashcard/AddFlashcard.js
@@ -2,13 +2,8 @@ import React, {useState} from "react"
 import Flashcard from "../Flashcard/Flashcard";
 
 const AddFlashcard = (props) => {
-    const [input, setInput] = useState(props.input)
-    //TODO problem se statem, posledni state zustava a udela se karta navic
     const handleClick = () => {
-        setInput(props.input)
-        console.log(props.input)
-        console.log(input)
-        const card = <Flashcard key={input.front} text={input}/>
+        const card = <Flashcard key={props.input.front} text={props.input}/>
         props.parentCallback(card)
     }
 
